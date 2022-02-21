50b1ba2184b3a5979370ebb0b434b6a3cd71edaa
aurelien-mk <aurelien.meltingk@gmail.com>
2022-02-21 17:02:51 +0100

    start menu

--- app/includes/menu.php
+++ app/includes/menu.php
@@ -1,31 +1,87 @@
 <div class="p-menu">
-    <ul class="p-nav_menu_nav">
-        <li class="<?php if($PAGE_name === "accueil") {echo 'active';} ?>">
-            <a href="<?php echo $NAV_accueil; ?>" title="<?php echo $NAV_TITLE_accueil; ?>">ACCUEIL</a>
-        </li>
-        <li class="<?php if($PAGE_name === "page_histoire") {echo 'active';} ?>">
-            <a href="<?php echo $NAV_page_histoire; ?>" title="<?php echo $NAV_TITLE_page_histoire; ?>">PAGE 2</a>
-        </li>
-        <li class="<?php if($PAGE_name === "page_carte") {echo 'active';} ?>">
-            <a href="<?php echo $NAV_page_carte; ?>" title="<?php echo $NAV_TITLE_page_carte; ?>">PAGE 3</a>
-        </li>
-        <li class="<?php if($PAGE_name === "page_rejoindre") {echo 'active';} ?>">
-            <a href="<?php echo $NAV_page_rejoindre; ?>" title="<?php echo $NAV_TITLE_page_rejoindre; ?>">PAGE 4</a>
-        </li>
-        <li class="<?php if($PAGE_name === "page5") {echo 'active';} ?>">
-            <a href="<?php echo $NAV_page5; ?>" title="<?php echo $NAV_TITLE_page5; ?>">PAGE 5</a>
-        </li>
-        <li class="<?php if($PAGE_name === "page6") {echo 'active';} ?>">
-            <a href="<?php echo $NAV_page6; ?>" title="<?php echo $NAV_TITLE_page6; ?>">PAGE 6</a>
-        </li>
-        <li class="<?php if($PAGE_name === "page7") {echo 'active';} ?>">
-            <a href="<?php echo $NAV_page7; ?>" title="<?php echo $NAV_TITLE_page7; ?>">PAGE 7</a>
-        </li>
-        <li class="<?php if($PAGE_name === "template-ville") {echo 'active';} ?>">
-            <a href="<?php echo $NAV_template-ville; ?>" title="<?php echo $NAV_TITLE_template-ville; ?>">PAGE 8</a>
-        </li>
-        <li class="<?php if($PAGE_name === "contact") {echo 'active';} ?>">
-            <a href="<?php echo $NAV_contact; ?>" title="<?php echo $NAV_TITLE_contact; ?>">Contact</a>
-        </li>
-    </ul>
+    <div class="p-menu_content heightJs">
+        <div class="p-menu_top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 col-md-5 col-md-push-6">
+                        <div class="content">
+                            <div class="p-menu_title">
+                                RÉSERVEZ VOTRE TABLE
+                            </div>
+                            <ul class="p-menu_nav">
+                                <li class="<?php if($PAGE_name === "page_carte") {echo 'active';} ?>">
+                                    <div class="wrap-item">
+                                        <a class="item" href="<?php echo $NAV_page_carte; ?>"
+                                            title="<?php echo $NAV_TITLE_page_carte; ?>">La carte</a>
+                                    </div>
+                                </li>
+                                <li class="<?php if($PAGE_name === "page5") {echo 'active';} ?>">
+                                    <div class="wrap-item">
+                                        <button class="item">
+                                            Nos restaurants
+                                        </button>
+                                    </div>
+                                </li>
+                            </ul>
+                            <div class="p-menu_bouton">
+                                <button class="bouton bg0-white-brRose">
+                                    <span>Réservation</span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-md-5 col-md-pull-5">
+                        <div class="content">
+                            <div class="p-menu_title">
+                                Découvrez Don Camillo
+                            </div>
+                            <ul class="p-menu_nav">
+                                <li class="<?php if($PAGE_name === "page_histoire") {echo 'active';} ?>">
+                                    <div class="wrap-item">
+                                        <a class="item" href="<?php echo $NAV_page_histoire; ?>"
+                                            title="<?php echo $NAV_TITLE_page_histoire; ?>">Une
+                                            histoire de famille</a>
+                                    </div>
+                                </li>
+                                <li class="<?php if($PAGE_name === "page_rejoindre") {echo 'active';} ?>">
+                                    <div class="wrap-item">
+                                        <a class="item" href="<?php echo $NAV_page_rejoindre; ?>"
+                                            title="<?php echo $NAV_TITLE_page_rejoindre; ?>">
+                                            Rejoindre la famille</a>
+                                    </div>
+                                </li>
+                            </ul>
+                            <div class="p-menu_bouton">
+                                <a href="" class="bouton bg0-white-brRose">
+                                    <span>le magazine</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="p-menu_bot">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 col-md-6">
+                        <a class="item item-contact semi-bold" href="<?php echo $NAV_contact; ?>"
+                            title="<?php echo $NAV_TITLE_contact; ?>">Nous
+                            contacter</a>
+                    </div>
+                    <div class="col-xs-12 col-md-6">
+                        <div class="p-menu_rs">
+                            <?php include "includes/rs.php"; ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <span class="forme-olivier">
+            <img src="img/svg/romarin-3-vert.svg" alt="Romarin Doncamillo">
+        </span>
+        <span class="forme-pates">
+            <img src="img/svg/assiette-2-vert.svg" alt="Pâtes Doncamillo">
+        </span>
+    </div>
 </div>
---
+++ app/includes/rs.php
@@ -0,0 +1,14 @@
+<article class="c-rs">
+    <a target="_blank" href="<?php echo $LINK_facebook; ?>" title="<?php echo $LINK_TITLE_facebook; ?>"
+        class="c-rs_item">
+        <span class="icon-facebook"></span>
+    </a>
+    <a target="_blank" href="<?php echo $LINK_instagram; ?>" title="<?php echo $LINK_TITLE_instagram; ?>"
+        class="c-rs_item">
+        <span class="icon-instagram"></span>
+    </a>
+    <a target="_blank" href="<?php echo $LINK_linkedin; ?>" title="<?php echo $LINK_TITLE_linkedin; ?>"
+        class="c-rs_item">
+        <span class="icon-linkedin"></span>
+    </a>
+</article>
